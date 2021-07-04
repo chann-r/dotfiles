bf704e2c634a22bfb8e6696d9851b61e75313d85
林　遼太朗 <hys.rtr.forit@gmail.com>
2021-07-04 13:28:00 +0900

    zsh系のプラグインを追加

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,1 +1,20 @@
+## テーマ
 zinit ice depth=1; zinit light romkatv/powerlevel10k
+
+
+## シンタックスハイライト
+zinit light zsh-users/zsh-syntax-highlighting
+
+
+## 履歴補完
+zinit light zsh-users/zsh-autosuggestions
+
+
+## catを見やすくする
+zinit ice as"program" from"gh-r" mv"bat* -> bat" pick"bat/bat"
+zinit light sharkdp/bat
+
+# 以下はただのエイリアス設定
+if builtin command -v bat > /dev/null; then
+  alias cat="bat"
+fi
