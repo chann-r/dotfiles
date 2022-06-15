be3827823afea38a5364e65be09db58c5e629b25
chann-r <hys.rtr.forit@gmail.com>
2022-06-15 12:10:38 +0900

    clipyとshiftltをwgetでインストール

--- initialize.sh
+++ initialize.sh
@@ -10,5 +10,9 @@ wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20B
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf -P ~/Library/Fonts/
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf -P ~/Library/Fonts/
 
+## 便利なアプリケーションをインストール
+wget https://github.com/Clipy/Clipy/releases/download/1.2.1/Clipy_1.2.1.dmg -P ~/Desktop/
+wget https://github.com/fikovnik/ShiftIt/releases/download/version-1.6.6/ShiftIt-1.6.6.zip -P ~/Desktop/
+
 ## 設定ファイルを設置
 ./iterm2/init.sh
